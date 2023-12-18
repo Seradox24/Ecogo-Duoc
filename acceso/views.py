1c1f83db30ff591cbab9a8d6a1bd6d559bfd4d26
sebfigueroap <82173094+sebfigueroap@users.noreply.github.com>
2023-12-18 19:16:16 -0300

    css

--- acceso/views.py
+++ acceso/views.py
@@ -3,3 +3,16 @@ from django.shortcuts import render
 # Create your views here.
 def acceso_login(request):
     return render(request, 'acceso/login.html')
+
+def login_view(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(request, username=username, password=password)
+
+        if user is not None:
+            login(request, user)
+            # Redirige al usuario a alguna página después del inicio de sesión exitoso
+            return HttpResponseRedirect('ruta-a-tu-pagina')
+
+    return render(request, 'login.html')
