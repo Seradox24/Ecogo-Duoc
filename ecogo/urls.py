07e7ad08679fff2c78eec2576ac3d89fe12194cb
Seradox24 <66537061+Seradox24@users.noreply.github.com>
2023-12-19 08:32:02 -0300

    19-12 static

--- ecogo/urls.py
+++ ecogo/urls.py
@@ -16,10 +16,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-
+from django.conf import settings
+from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('acceso.urls'),name="acceso_urls"),
     path('', include('alumno.urls'),name="alumno_urls"),
     
-]
+]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
