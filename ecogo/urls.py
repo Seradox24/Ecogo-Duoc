d3c1f35fd0b0ad6bd2f767679049cc0abea23782
Seradox24 <66537061+Seradox24@users.noreply.github.com>
2023-12-19 07:55:22 -0300

    test

--- ecogo/urls.py
+++ ecogo/urls.py
@@ -20,5 +20,6 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('acceso.urls'),name="acceso_urls"),
+    path('', include('alumno.urls'),name="alumno_urls"),
     
 ]
