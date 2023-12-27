21e2a09a7b6278ae84c4a7a4c1055dcab8d09d0e
sebfigueroap <82173094+sebfigueroap@users.noreply.github.com>
2023-12-27 19:09:53 -0300

    urls docente

---
+++ docente/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path, include
+from .views import *
+
+urlpatterns = [
+	path('home/', home_docente, name="home_docente"),
+	path('j-inasistencia/', ji_docente, name="ji_docente"),
+	path('mis-salida/', msalida_docente, name="msalida_docente"),
+    path('gest-users/', gest_users, name="gest_users")
+]
---
+++ docente/views.py
@@ -0,0 +1,15 @@
+from django.shortcuts import render
+
+# Create your views here.
+def home_docente(request):
+    return render(request, 'db_docente/db_home_d.html')
+
+
+def ji_docente(request):
+    return render(request, 'db_docente/db_docente_asis.html')
+
+def msalida_docente(request):
+    return render(request, 'db_docente/db_docente_mysali.html')
+
+def gest_users(request):
+    return render(request, 'db_docente/db_docente_gest_users.html')
