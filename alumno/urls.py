4de0a9148cddaf8ea803eb7d0cfd1d293221ee3e
Seradox24 <ma.inayao@alumnos.duoc.cl>
2023-12-27 17:41:18 -0300

    mati/home-asistencia-listsalidas

--- alumno/urls.py
+++ alumno/urls.py
@@ -3,4 +3,6 @@ from .views import *
 
 urlpatterns = [
 	path('home/', home_alumno, name="home_alumno"),
+	path('j-inasistencia/', ji_alumno, name="ji_alumno"),
+	path('mis-salida/', msalida_alumno, name="msalida_alumno"),
 ]
--- alumno/views.py
+++ alumno/views.py
@@ -5,4 +5,11 @@ from django.shortcuts import render
 def home_alumno(request):
     return render(request, 'db_alumno/db_home.html')
 
+
+def ji_alumno(request):
+    return render(request, 'db_alumno/db_alumno_asis.html')
+
+def msalida_alumno(request):
+    return render(request, 'db_alumno/db_alumno_mysali.html')
+
 # Create your views here.
