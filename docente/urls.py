6d7e92559679d23bd59e722b8771ec83e3f335e6
Seradox24 <ma.inayao@alumnos.duoc.cl>
2024-01-02 22:04:09 -0300

    Dejar de rastrear archivos según .gitignore

--- docente/urls.py
+++ docente/urls.py
@@ -7,9 +7,7 @@ urlpatterns = [
 	path('j-inasistencia/', ji_docente, name="ji_docente"),
 	path('mis-salida/', msalida_docente, name="msalida_docente"),
     path('gest-users/', gest_users, name="gest_users"),
-
-	path('crear-salida/', crear_salida,  name="crear_salida")
-
+	path('crear-salida/', crear_salida,  name="crear_salida"),
     path('agreg-asig/', agreg_asig, name="agreg_asig"),
     path('gest-asig/', gest_asig, name='gest_asig'),
     path('guardar_asignatura/', views.guardar_asignatura, name='guardar_asignatura'),
