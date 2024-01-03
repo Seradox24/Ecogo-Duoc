e3830565e69843d4d2f537c88a69a23454be4233
Seradox24 <ma.inayao@alumnos.duoc.cl>
2024-01-03 15:16:48 -0300

    permisos fuera

--- docente/views.py
+++ docente/views.py
@@ -6,23 +6,20 @@ from core.decorators import Docente_required
 from django.contrib.auth.decorators import login_required
 
 # Create your views here.
-@login_required
-@Docente_required
+
+
 def home_docente(request):
     return render(request, 'db_docente/db_home_d.html')
 
-@login_required
-@Docente_required
+
 def ji_docente(request):
     return render(request, 'db_docente/db_docente_asis.html')
 
-@login_required
-@Docente_required
+
 def msalida_docente(request):
     return render(request, 'db_docente/db_docente_mysali.html')
 
-@login_required
-@Docente_required
+
 def gest_users(request):
     return render(request, 'db_docente/db_docente_gest_users.html')
 
