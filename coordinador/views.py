3e8bfb333294a0406e07bf1666fd07dfcf6b7052
sebfigueroap <82173094+sebfigueroap@users.noreply.github.com>
2024-01-08 19:06:52 -0300

    avance de formulario

--- coordinador/views.py
+++ coordinador/views.py
@@ -14,13 +14,14 @@ def gest_users(request):
 
 
 def crear_salida(request):
+    mensaje = ""  # Inicializa la variable con un valor predeterminado
     if request.method == 'POST':
         form = SalidaTerrenoForm(request.POST)
         if form.is_valid():
             form.save()
-            
-            return redirect('db_coordinador/db_home_c.html')
+            return render(request, 'db_coordinador/db_coordinador_crear_sl.html', {'form': form, 'success': True})
     else:
         form = SalidaTerrenoForm()
-    return render(request, 'db_coordinador/db_coordinador_crear_sl.html', {'form': form})
+    return render(request, 'db_coordinador/db_coordinador_crear_sl.html', {'form': form, 'mensaje': mensaje})
+
 
