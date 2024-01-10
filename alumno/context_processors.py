52a8f634755f90656e3675cb1fbd7cfe3101a8a1
Seradox24 <ma.inayao@alumnos.duoc.cl>
2024-01-10 20:36:12 -0300

    supernav-mati

---
+++ alumno/context_processors.py
@@ -0,0 +1,47 @@
+from django.contrib.auth.models import AnonymousUser
+from core.models import UsersMetadata
+from django.core.exceptions import ObjectDoesNotExist
+
+def urls_processor(request):
+    user = request.user
+
+    # Verificar si el usuario está autenticado
+    if not isinstance(user, AnonymousUser):
+        try:
+            user_metadata = UsersMetadata.objects.get(user=user)
+        except ObjectDoesNotExist:
+            user_metadata = None
+
+        if user_metadata and hasattr(user_metadata, 'perfil') and hasattr(user_metadata.perfil, 'nombre'):
+            perfil_nombre = user_metadata.perfil.nombre
+
+            if perfil_nombre == 'Alumno':
+                urls = [
+                    {'url': 'home_alumno', 'nombre': 'Home', 'icon': 'fs-5 fa fa-house'},
+                    {'url': 'ji_alumno', 'nombre': 'Justificación de Inasistencia', 'icon': 'txt fa fa-heart-circle-plus'},
+                    {'url': 'msalida_alumno', 'nombre': 'Mis Salidas', 'icon': 'txt fa fa-person-walking-luggage'},
+                    # Añade más URLs para el perfil de alumno aquí
+                ]
+            elif perfil_nombre == 'Docente':
+                urls = [
+                    {'url': 'home_docente', 'nombre': 'Home', 'icon': 'fs-5 fa fa-house'},
+                    {'url': 'gest_users', 'nombre': 'Gestionar Usuarios', 'icon': 'txt fa fa-users-gear'},
+
+                    # Añade las URLs para el perfil de docente aquí
+                ]
+            elif perfil_nombre == 'Coordinador':
+                urls = [
+                    # Añade las URLs para el perfil de coordinador aquí
+                ]
+            elif perfil_nombre == 'Pañol':
+                urls = [
+                    # Añade las URLs para el perfil de pañol aquí
+                ]
+            else:
+                urls = []  # Si el usuario no tiene un rol reconocido, no proporcionamos ninguna URL
+        else:
+            urls = []  # Si el usuario no tiene UsersMetadata, perfil o nombre, no proporcionamos ninguna URL
+    else:
+        urls = []  # Si el usuario no está autenticado, no proporcionamos ninguna URL
+
+    return {'urls': urls}
--- alumno/views.py
+++ alumno/views.py
@@ -2,10 +2,13 @@ from django.shortcuts import render
 from core.models import UsersMetadata, Perfiles
 from core.decorators import Alumno_required
 from django.contrib.auth.decorators import login_required
+import json
 
 
 def home_alumno(request):
+    paginas = "home_alumno"
     user = request.user
+    
     user_metadata = UsersMetadata.objects.get(user=user)
     return render(request, 'db_alumno/db_home.html')
 
