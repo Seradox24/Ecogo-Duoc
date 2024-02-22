764feece47a4d85726e8f2842c4dc29332fe87f5
Seradox24 <ma.inayao@alumnos.duoc.cl>
2024-02-22 02:52:45 -0300

    bajada

---
+++ alumno/forms.py
@@ -0,0 +1,28 @@
+from django import forms
+from .models import BajaEstudiante, Bajada
+from coordinador.models import SalidaTerreno
+from django.db import models
+
+
+
+class BajaEstudianteForm(forms.ModelForm):
+    # Sobrescribimos el método __init__ para personalizar el formulario
+    def __init__(self, *args, **kwargs):
+        # Extraemos el id de la salida a terreno si se proporciona
+        salida_terreno_id = kwargs.pop('salida_terreno_id', None)
+        # Llamamos al __init__ original de ModelForm
+        super(BajaEstudianteForm, self).__init__(*args, **kwargs)
+        # Si se proporcionó un id de salida a terreno
+        if salida_terreno_id:
+            # Filtramos las bajadas que no tienen salida a terreno o que tienen la salida a terreno especificada
+            self.fields['bajada'].queryset = Bajada.objects.filter(
+                models.Q(salida_terreno__isnull=True) | 
+                models.Q(salida_terreno__id=salida_terreno_id)
+            )
+        else:
+            # Si no se proporcionó un id de salida a terreno, mostramos solo las bajadas que no tienen salida a terreno
+            self.fields['bajada'].queryset = Bajada.objects.filter(salida_terreno__isnull=True)
+
+    class Meta:
+        model = BajaEstudiante
+        fields = ['bajada']
--- alumno/models.py
+++ alumno/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from core.models import *
 from coordinador.models import *
 
+
 class Estado(models.Model):
     nombre = models.CharField(max_length=50)
 
@@ -24,6 +25,25 @@ class Documento_inasis(models.Model):
 
     def __str__(self):
         return self.nombre
+
+
+class Bajada(models.Model):
+    nombre = models.CharField(max_length=200,default='', null=True)
+    descripcion = models.CharField(max_length=200,default='', null=True)
+    salida_terreno = models.ForeignKey(SalidaTerreno, models.CASCADE, blank=True, null=True)
+
+    def __str__(self):
+        return self.nombre
+
+class BajaEstudiante(models.Model):
+    estudiante = models.ForeignKey(UsersMetadata, models.CASCADE, related_name='bajas',default='', null=True )
+    bajada = models.ForeignKey(Bajada, models.CASCADE, related_name='estudiantes',default='', null=True)
+    fecha_creacion = models.DateTimeField(auto_now_add=True)
+    salida_terreno = models.ForeignKey(SalidaTerreno, models.CASCADE, blank=True, null=True)
+
+    def __str__(self):
+        return f'-{self.estudiante} - {self.bajada}'
+
 
 
 # Create your models here.
