3ca89717d7fe6852990259ae9ee8c72f7913e0ca
Seradox24 <ma.inayao@alumnos.duoc.cl>
2024-04-04 06:36:09 -0300

    fix docente

--- docente/views.py
+++ docente/views.py
@@ -17,7 +17,42 @@ from alumno.models import *
 @login_required
 @Docente_required
 def home_docente(request):
-    return render(request, 'db_docente/db_home_d.html')
+    try:
+        # Obtener el objeto Perfil con el nombre 'Alumno'
+        perfil_alumno = Perfiles.objects.get(nombre='Alumno')
+        # Contar todos los usuarios con el perfil de alumno
+        cantidad_alumnos_g = UsersMetadata.objects.filter(perfil=perfil_alumno).count()
+        # Contar todas las instancias de SalidaTerreno
+        cantidad_salidas_g = SalidaTerreno.objects.count()
+        # Contar todas las instancias de Asignatura
+        cantidad_asignaturas_g = Asignatura.objects.count()
+        # Contar todas las instancias de Seccion
+        cantidad_secciones_g = Seccion.objects.count()
+
+        # print("Cantidad total de secciones:", cantidad_secciones)
+        # print("Cantidad de alumnos:", cantidad_alumnos)
+        # print("Cantidad total de salidas a terreno:", cantidad_salidas)
+        # print("Cantidad total de asignaturas:", cantidad_asignaturas)
+
+        # Nuevo bloque para calcular la cantidad de secciones y alumnos por sección por asignatura
+        salidas = SalidaTerreno.objects.all().order_by('fecha_ingreso')
+        # Aquí se obtienen todas las salidas a terreno disponibles
+
+        contexto = {
+            'cantidad_alumnos': cantidad_alumnos_g,
+            'cantidad_salidas': cantidad_salidas_g,
+            'cantidad_asignaturas': cantidad_asignaturas_g,
+            'cantidad_secciones': cantidad_secciones_g,
+            'salidas': salidas
+        }
+    except Perfiles.DoesNotExist:
+        # Si el perfil 'Alumno' no existe, manejar la excepción
+        contexto = {'error': 'El perfil de alumno no está definido.'}
+    except Exception as e:
+        # Cualquier otra excepción se maneja aquí
+        contexto = {'error': f'Ha ocurrido un error: {e}'}
+
+    return render(request, 'db_coordinador/db_home_c.html', contexto)
 
 
 @login_required
